efff582dd01426e6c61a28f7c44edb5dee58ca79
Ernest Irabor <irabeny89@gmail.com>
2020-08-31 21:02:50 +0100

    prevent empty input

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,14 @@
-addTask = () => {
-  const listItem = document.createElement('li')
-  createHTMLTask(listItem, task.value)
-  document.querySelector('#taskList').appendChild(listItem)
-}
-
 createHTMLTask = (listItem, input) => {
   listItem.innerHTML = `<input type="image" src="../assets/images/delete btn.png" alt="trash-icon">${input}<input id="checkbox" type="checkbox">`
   listItem
+}
+
+addTask = () => {
+  const listItem = document.createElement('li')
+  if (task.value) {
+    createHTMLTask(listItem, task.value)
+    document.querySelector('#taskList').appendChild(listItem)
+  }
 }
 
 addButtonImage.addEventListener('click', addTask)
